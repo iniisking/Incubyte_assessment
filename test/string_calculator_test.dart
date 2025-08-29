3f50dddb0bee6d0614bfa64afa678afdeecd030d
IniOluwa Longe <inilonge@gmail.com>
2025-08-29 02:13:26 +0100

    first commit

---
+++ test/string_calculator_test.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+import 'package:string_calculator/string_calculator.dart';
+
+void main() {
+  test('add empty string returns 0', () {
+    expect(add(''), equals(0));
+  });
+}
